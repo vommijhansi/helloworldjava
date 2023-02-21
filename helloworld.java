81a437e5bbc017db624f111ea687982d23124602
vommijhansi <125142054+vommijhansi@users.noreply.github.com>
2023-02-22 00:49:44 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 22th 2023 - 12:44am");
+    System.out.println("Hello World - Feb 22th 2023 - 12:49am");
     }
 }
